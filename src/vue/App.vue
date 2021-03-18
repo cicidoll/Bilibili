55469cb6eb7ccac35e6722bedeb34e490a35aa45
cicidoll <ayaseemt@qq.com>
2021-03-19 00:19:30 +0800

    2021-3-18

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -136,191 +136,7 @@
             </ul>
           </div>
           <span class="tab-line-itnl"></span>
-          <div id="primaryChannelMenu" class="channel-menu-itnl report-wrap-module"></div>
-          <div id="primaryChannelMenu" class="channel-menu-itnl report-wrap-module">
-            <span>
-              <div role="tooltip" id="van-popover-1953" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:162px;display:none;" tabindex="0">
-                <!---->
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/douga/mad/" class="name">MAD·AMV</a>
-                    <a href="//www.bilibili.com/v/douga/mmd/" class="name">MMD·3D</a>
-                    <a href="//www.bilibili.com/v/douga/voice/" class="name">短片·手书·配音</a>
-                    <a href="//www.bilibili.com/v/douga/garage_kit/" class="name">手办·模玩</a>
-                    <a href="//www.bilibili.com/v/douga/tokusatsu/" class="name">特摄</a>
-                    <a href="//www.bilibili.com/v/douga/other/" class="name">综合</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-1953" tabindex="0">
-                <a href="//www.bilibili.com/v/douga/" class="name">
-                  <span>动画<em>999+</em></span>
-                </a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-3004" tabindex="0">
-                <a href="//www.bilibili.com/anime/" class="name"><span>番剧<em>377</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-3071" tabindex="0">
-                <a href="//www.bilibili.com/v/music/" class="name">
-                  <span>音乐<em>999+</em></span>
-                </a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-4368" tabindex="0">
-                <a href="//www.bilibili.com/guochuang/" class="name">
-                  <span>国创<em>803</em></span>
-                </a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-3650" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:172px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/dance/otaku/" class="name">宅舞</a>
-                    <a href="//www.bilibili.com/v/dance/hiphop/" class="name">街舞</a>
-                    <a href="//www.bilibili.com/v/dance/star/" class="name">明星舞蹈</a>
-                    <a href="//www.bilibili.com/v/dance/china/" class="name">中国舞</a>
-                    <a href="//www.bilibili.com/v/dance/three_d/" class="name">舞蹈综合</a>
-                    <a href="//www.bilibili.com/v/dance/demo/" class="name">舞蹈教程</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-3650" tabindex="0">
-                <a href="//www.bilibili.com/v/dance/" class="name"><span>舞蹈<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-1517" tabindex="0">
-                <a href="//www.bilibili.com/v/game/" class="name"><span>游戏<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-7227" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:172px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/technology/science/" class="name">科学科普</a>
-                    <a href="//www.bilibili.com/v/technology/fun/" class="name">社科人文</a>
-                    <a href="//www.bilibili.com/v/technology/finance/" class="name">财经</a>
-                    <a href="//www.bilibili.com/v/technology/campus/" class="name">校园学习</a>
-                    <a href="//www.bilibili.com/v/technology/career/" class="name">职业职场</a>
-                    <a href="//www.bilibili.com/v/technology/wild/" class="name">野生技术协会</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-7227" tabindex="0">
-                <a href="//www.bilibili.com/v/technology/" class="name"><span>知识<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-1301" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:80px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/digital/mobile/" class="name">手机平板</a>
-                    <a href="//www.bilibili.com/v/digital/pc/" class="name">电脑装机</a>
-                    <a href="//www.bilibili.com/v/digital/photography/" class="name">摄影摄像</a>
-                    <a href="//www.bilibili.com/v/digital/intelligence_av/" class="name">影音智能</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-1301" tabindex="0">
-                <a href="//www.bilibili.com/v/digital/" class="name"><span>数码<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-4042" tabindex="0">
-                <a href="//www.bilibili.com/v/life/" class="name"><span>生活<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-3374" tabindex="0">
-                <a href="//www.bilibili.com/v/kichiku/" class="name"><span>鬼畜<em>186</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div class="item van-popover__reference" aria-describedby="van-popover-6827" tabindex="0">
-                <a href="//www.bilibili.com/v/fashion/" class="name"><span>时尚<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-8729" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:60px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/information/hotspot/" class="name">热点</a>
-                    <a href="//www.bilibili.com/v/information/global/" class="name">环球</a>
-                    <a href="//www.bilibili.com/v/information/social/" class="name">社会</a>
-                    <a href="//www.bilibili.com/v/information/multiple/" class="name">综合</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-8729" tabindex="0">
-                <a href="//www.bilibili.com/v/information/" class="name"><span>资讯<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-2133" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:62px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/ent/variety/" class="name">综艺</a>
-                    <a href="//www.bilibili.com/v/ent/star/" class="name">明星</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-2133" tabindex="0">
-                <a href="//www.bilibili.com/v/ent/" class="name"><span>娱乐<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-9521" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:84px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/cinephile/cinecism/" class="name">影视杂谈</a>
-                    <a href="//www.bilibili.com/v/cinephile/montage/" class="name">影视剪辑</a>
-                    <a href="//www.bilibili.com/v/cinephile/shortfilm/" class="name">短片</a>
-                    <a href="//www.bilibili.com/v/cinephile/trailer_info/" class="name">预告·资讯</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-9521" tabindex="0">
-                <a href="//www.bilibili.com/v/cinephile/" class="name"><span>影视<em>999+</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-9173" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width:68px;display:none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/documentary/" class="name">纪录片</a>
-                    <a href="//www.bilibili.com/movie/" class="name">电影</a>
-                    <a href="//www.bilibili.com/tv/" class="name">电视剧</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-9173" tabindex="0">
-                <a href="//www.bilibili.com/cinema/" class="name"><span>放映厅<em>193</em></span></a>
-              </div>
-            </span>
-            <span>
-              <div role="tooltip" id="van-popover-37" aria-hidden="true" class="van-popover van-popper van-popper-channel" style="width: 162px; display: none;" tabindex="0">
-                <div class="sub-container">
-                  <div class="sub-item">
-                    <a href="//www.bilibili.com/v/life/funny" class="name">搞笑</a>
-                    <a href="//www.bilibili.com/v/food" class="name">美食</a>
-                    <a href="//www.bilibili.com/v/animal" class="name">动物圈</a>
-                    <a href="//www.bilibili.com/v/game/stand_alone" class="name">单机游戏</a>
-                    <a href="//www.bilibili.com/v/life/daily/#/530003" class="name">VLOG</a>
-                  </div>
-                </div>
-              </div>
-              <div class="item van-popover__reference" aria-describedby="van-popover-37" tabindex="0">
-                <a href="javascript:;" class="name"><span>更多<em style="display: none;">--</em></span>
-                <i class="bilifont bili-icon_caozuo_xiangyou-copy"></i></a>
-              </div>
-            </span>
-          </div>
+          <primary-channel-menu class="channel-menu-itnl"></primary-channel-menu>
           <span class="tab-line-itnl none"></span>
           <div id="primaryFriendshipLink" class="friendship-link report-wrap-module"></div>
         </div>
@@ -335,12 +151,13 @@
 <script>
 import userConSignin from '../components/international-header/user-con-signin.vue'
 import biliBanner from '../components/international-header/bili-banner.vue'
-
+import primaryChannelMenu from '../components/international-header/primary-channel-menu.vue'
 
 export default {
   components: {
     'user-con-signin': userConSignin,
-    'bili-banner': biliBanner
+    'bili-banner': biliBanner,
+    'primary-channel-menu':primaryChannelMenu
   }
 }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,30 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  port: 3000,
+  hostname: "bilibili.com",
+  // server:{
+  //   '/api': {
+  //     target: 'http://api.bilibili.com',
+  //     changeOrigin: true,
+  //     rewrite: (path) => path.replace(/^\/api/, '')
+  //   }
+  // }
+  // proxy:{
+  //   '/api': {
+  //     target: 'http://api.bilibili.com/',
+  //     changeOrigin: true,
+  //     rewrite: (path) => path.replace(/^\/api/, '')
+  //   }
+  // }
+  server: {
+    proxy:{
+      '/api': {
+        target: 'http://api.bilibili.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
